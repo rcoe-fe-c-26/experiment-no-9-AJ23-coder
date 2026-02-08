8b21345079d287d5e5d63b95fe1c4deb2e390268
AJ23-coder <aayush338jha@eng.rizvi.edu.in>
2026-02-08 23:03:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
+# Coder: Aayush Jha 
 # Date:
 
 print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+factorial = 1 
+ 
+num=int(input("Enter the number:"))   
+
+for x in range(1,(num+1)):
+    factorial=factorial*x
+    
+print(factorial)
+    
