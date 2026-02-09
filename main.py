6656b589b0b69e7f968cf6f3eb91878f1a3ce011
AJ23-coder <aayush338jha@eng.rizvi.edu.in>
2026-02-09 10:20:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
 # Coder: Aayush Jha 
-# Date: 
+# Date: 30 january 2026
 
 print("--- Factorial Finder ---\n")
 
@@ -9,12 +9,13 @@ print("--- Factorial Finder ---\n")
 # Write your code here
 num=int(input("Enter the number:"))   
 factorial=1
-if n>=0:
-    for n in range(1,(num+1)):
+if(n>=0):
+    for n in range(1,num+1):
         factorial=factorial*n
     print(f"Factorial of {num} is {factorial}")
 else:
     print(f"Factorial of {num} is Not Defined")
+
 
 
 
